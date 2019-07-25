baeed4ebfabf62e755ce41aeefe682c1d40e1c83
ajsherrell <amiesherrell@gmail.com>
2019-07-25 17:53:23 -0400

    add list layouts with recyclerview

--- app/src/main/java/com/ajsherrell/hyperbaricquiz/CategoryListActivity.java
+++ app/src/main/java/com/ajsherrell/hyperbaricquiz/CategoryListActivity.java
@@ -3,7 +3,7 @@ package com.ajsherrell.hyperbaricquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CategoryLIstActivity extends AppCompatActivity {
+public class CategoryListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
